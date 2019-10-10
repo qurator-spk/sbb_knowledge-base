9ed2f31b6410939a9fcfc1586fd29f8851f6a61c
Gerber, Mike <mike.gerber@sbb.spk-berlin.de>
2019-10-10 16:11:59 +0200

    🧹 sbb_textline_docker/textline_detector: Remove duplicate code

--- qurator/textline_detector/make.sh
+++
@@ -1,9 +0,0 @@
-all: build install
-
-build:
-       python3 setup.py build
-
-install:
-       python3 setup.py install --user
-       cp textlineerkenner/textlineerkenner.py ~/bin/textlineerkenner
-       chmod +x ~/bin/textlineerkenner
--- qurator/textline_detector/textlineerkenner/__init__.py
+++
@@ -1,1 +0,0 @@
-from textlinerkenner import *
