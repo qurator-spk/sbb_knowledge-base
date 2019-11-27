f2d913d20549dee20c6a9f0a115ff699668c8725
Kai Labusch <Kai.Labusch@sbb.spk-berlin.de>
2019-11-27 16:19:14 +0100

    ALTO NER annotation

--- qurator/sbb/xml.py
+++ qurator/sbb/xml.py
@@ -10,15 +10,19 @@ import pandas as pd
 import json
 import unicodedata
 from qurator.utils.parallel import run as prun
+import logging
 
 with warnings.catch_warnings():
     warnings.simplefilter("ignore")
 
+logger = logging.getLogger(__name__)
 
-def alto_iterate_string_elements(xml_file):
 
-    tree = ElementTree.parse(xml_file)
-    root = tree.getroot()
+def alto_iterate_string_elements(xml_file=None, root=None):
+
+    if root is None:
+        tree = ElementTree.parse(xml_file)
+        root = tree.getroot()
 
     for string_elem in root.iter('{http://www.loc.gov/standards/alto/ns-v2#}String'):
         if 'WC' in string_elem.attrib:
@@ -31,7 +35,7 @@ def alto_iterate_string_elements(xml_file):
         else:
             content = str(np.NAN)
 
-        yield content, wc, string_elem
+        yield unicodedata.normalize('NFC', content), wc, string_elem
 
 
 def alto_xml_files_from_dir(source_dir):
@@ -50,6 +54,56 @@ def alto_xml_files_from_dir(source_dir):
             yield ppn, filename
 
 
+def alto_add_entity_map(root, entity_map):
+
+    if len(entity_map) == 0:
+        return
+
+    root.attrib['{http://www.w3.org/2001/XMLSchema-instance}schemaLocation'] = \
+        'http://www.loc.gov/standards/alto/ns-v2# http://www.loc.gov/standards/alto/alto.xsd'
+
+    tags = ElementTree.Element('ns0:Tags')
+
+    layout = root.find('.//{http://www.loc.gov/standards/alto/ns-v2#}Layout')
+
+    if layout is None:
+        return
+
+    tags_pos = [c for c in root.getchildren()].index(layout)
+
+    root.insert(tags_pos, tags)
+
+    for entity_id, (entity_label, entity_type) in entity_map.items():
+        named_entity_elem = ElementTree.Element('ns0:NamedEntityTag')
+
+        try:
+            assert entity_type in {'PER', 'LOC', 'ORG'}
+        except AssertionError:
+            import ipdb;ipdb.set_trace()
+
+        named_entity_elem.set('ID', entity_type + str(entity_id))
+        named_entity_elem.set('LABEL', entity_label)
+
+        tags.append(named_entity_elem)
+
+
+def alto_add_entity_references(entity_map, tagged_contents):
+
+    for string_elem, entity_id in tagged_contents:
+
+        if entity_id is None:
+            continue
+
+        entity_label, entity_type = entity_map[entity_id]
+
+        try:
+            assert entity_type in {'PER', 'LOC', 'ORG'}
+        except AssertionError:
+            import ipdb;ipdb.set_trace()
+
+        string_elem.set('TAGREFS', entity_type + str(entity_id))
+
+
 class ExtractTask:
 
     def __init__(self, source_dir, ppn,  filename):
@@ -65,7 +119,7 @@ class ExtractTask:
             word_confidences = []
 
             for content, wc, _ in \
-                    alto_iterate_string_elements(self._source_dir + '/' + self._ppn + '/' + self._filename):
+                    alto_iterate_string_elements(xml_file=self._source_dir + '/' + self._ppn + '/' + self._filename):
 
                 string_contents.append(content)
                 word_confidences.append(wc)
@@ -163,53 +217,129 @@ class AnnotateTask:
                                        params=(self._ppn, self._filename))
 
                 if len(df) == 0:
-                    print('Not found: {}, {}', self._ppn, self._filename)
+                    logger.info('Not found: {}, {}'.format(self._ppn, self._filename))
                     return None, None
 
-            ner_text = [unicodedata.normalize('NFC', w) for s in json.loads(df.text.iloc[0]) for w in s]
-            ner_tags = [t for s in json.loads(df.tags.iloc[0]) for t in s]
+            tree = ElementTree.parse(self._source_dir + '/' + self._ppn + '/' + self._filename)
+            root = tree.getroot()
 
-            string_contents = []
-            word_confidences = []
-            string_elements = []
+            entity_list, tagged_contents = AnnotateTask.tag_content(root, df)
 
-            for content, wc, string_elem in \
-                    alto_iterate_string_elements(self._source_dir + '/' + self._ppn + '/' + self._filename):
+            entity_map, tagged_contents = AnnotateTask.remap_entities(entity_list, tagged_contents)
 
-                string_contents.append(unicodedata.normalize('NFC', content))
-                word_confidences.append(wc)
-                string_elements.append(string_elem)
+            alto_add_entity_map(root, entity_map)
 
-            assert len(ner_text) >= len(string_contents)
+            alto_add_entity_references(entity_map, tagged_contents)
 
-            tagged_string_contents = []
-            ner_pos = 0
-            for content, wc, string_elem in zip(string_contents, word_confidences, string_elements):
+            ppn_path = self._dest_dir + '/' + self._ppn
+            if not os.path.exists(ppn_path):
+                os.makedirs(ppn_path, exist_ok=True)
 
-                if not content.startswith(ner_text[ner_pos]):
-                    continue
-
-                ner_word = ner_text[ner_pos]
-                ner_tag = {ner_tags[ner_pos]}
-                ner_pos += 1
-
-                while ner_word != content:
-                    ner_word += ner_text[ner_pos]
-                    ner_tag.add(ner_tags[ner_pos])
-                    ner_pos += 1
-
-                tagged_string_contents.append((content, ner_tag))
-
-            import ipdb;ipdb.set_trace()
+            tree.write(ppn_path + '/' + self._filename)
 
             return self._filename, self._ppn
 
-        except Exception as e:
+        except Exception as exx:
+            import ipdb;ipdb.set_trace()
 
-            raise e
-            # print(e)
-            # print(self._ppn, self._filename, self._source_dir, self._dest_dir)
-            # return None, None
+            logger.error(exx)
+
+    @staticmethod
+    def tag_content(root, ner_data):
+
+        entity_list = list()
+
+        def iterate_ner_tags():
+
+            nonlocal entity_list
+
+            _text = json.loads(ner_data.text.iloc[0])
+            _tags = json.loads(ner_data.tags.iloc[0])
+
+            eid = 0
+            for sen_tok, sen_tag in zip(_text, _tags):
+                entity = list()
+                entity_type = set()
+                for tok, tag in zip(sen_tok, sen_tag):
+
+                    assert len(entity_type) < 2
+
+                    tag = 'O' if tag == 'O' else tag[2:]
+
+                    try:
+                        assert tag in {'O', 'PER', 'LOC', 'ORG'}
+                    except AssertionError:
+                        import ipdb;
+                        ipdb.set_trace()
+
+                    if len(entity) > 0 and tag not in entity_type:
+                        entity_list[-1] = (eid, " ".join(entity), list(entity_type)[0])
+                        eid += 1
+                        entity = list()
+                        entity_type = set()
+
+                    if tag != 'O':
+                        if len(entity) == 0:
+                            entity_list.append((eid, "", None))
+
+                        entity += [tok]
+                        entity_type.add(tag)
+
+                        entity_list[-1] = (eid, " ".join(entity), list(entity_type)[0])
+
+                    yield unicodedata.normalize('NFC', tok), eid if tag != 'O' else None
+
+        ner_sequence = iterate_ner_tags()
+        tagged_contents = []
+        ner_concat = ''
+        for content, wc, string_elem in alto_iterate_string_elements(root=root):
+
+            entity_ids = set()
+            while content != ner_concat:
+
+                assert len(content) >= len(ner_concat)
+
+                ner_text, entity_id = next(ner_sequence)
+
+                ner_concat += ner_text
+
+                if entity_id is not None:
+                    entity_ids.add(entity_id)
+
+            tagged_contents.append((string_elem, entity_ids))
+            ner_concat = ''
+
+        return entity_list, tagged_contents
+
+    @staticmethod
+    def remap_entities(entity_list, tagged_contents):
+
+        entity_label_to_id = dict()
+        entity_map = dict()
+
+        id_map = dict()
+        for entity_id, entity_label, entity_type in entity_list:
+
+            if entity_label in entity_label_to_id:
+                id_map[entity_id] = entity_label_to_id[entity_label]
+            else:
+                entity_label_to_id[entity_label] = entity_id
+                id_map[entity_id] = entity_id
+
+                entity_map[entity_id] = (entity_label, entity_type)
+
+        tagged_contents_tmp = []
+        for string_elem, entity_ids in tagged_contents:
+
+            if len(entity_ids) == 0 or len(entity_ids) > 1:
+                tagged_contents_tmp.append((string_elem, None))
+                continue
+
+            entity_id = list(entity_ids)[0]
+
+            tagged_contents_tmp.append((string_elem, id_map[entity_id]))
+
+        return entity_map, tagged_contents_tmp
 
     @staticmethod
     def initialize(tagged_sqlite_file):
@@ -239,10 +369,7 @@ def altoannotator(tagged_sqlite_file, source_dir, dest_dir, processes):
 
     for ppn, filename, in prun(AnnotateTask.get_all(source_dir, dest_dir), processes=processes,
                                initializer=AnnotateTask.initialize, initargs=(tagged_sqlite_file,)):
-
         if ppn is None:
             continue
 
-        print(ppn, filename)
-
-        break
+        # print(ppn, filename)
